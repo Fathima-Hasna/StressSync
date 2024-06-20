2982eeb977b61662e448400d5cf9a5f1ae8218b6
Arisha <arishak851@gmail.com>
2024-06-21 03:09:52 +0400

    .

---
+++ app/src/main/java/com/example/algo/pulserate.java
@@ -0,0 +1,34 @@
+package com.example.algo;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class pulserate extends AppCompatActivity {
+
+     private TextView pulse_rate;
+     private Button show_pulse;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pulserate);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        pulse_rate=findViewById(R.id.pulse_rate_disp);
+        show_pulse=findViewById(R.id.button);
+
+    }
+}
