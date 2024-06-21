acf65a4ea26b008c830a921e9cc75d5f6d3a9ad6
Arisha <arishak851@gmail.com>
2024-06-21 12:15:13 +0400

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation ("com.github.PhilJay:MPAndroidChart:3.1.0")
+    implementation(libs.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
